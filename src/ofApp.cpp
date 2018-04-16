3ff85a496a27cea63826ae523f206828ee1a4868
Leonardo <ll10@illinois.edu>
2018-04-16 00:33:02 -0500

    Added background color and drawPoints method

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,8 +1,11 @@
 #include "ofApp.h"
 
+const int num_pts = 324;
+ofVec2f points[num_pts];
+int pt_spacing = 15;
 
 void ofApp::setup() {
-
+	setupPointCoord();
 }
 
 void ofApp::update() {
@@ -10,5 +13,28 @@ void ofApp::update() {
 }
 
 void ofApp::draw() {
+	ofBackground(r_bg, g_bg, b_bg);
+	drawPoints();
+}
 
+void ofApp::drawPoints() {
+	int dimension = pt_spacing * (sqrt(num_pts));
+	ofPushMatrix();
+	ofTranslate((ofGetWidth() - dimension) / 2, (ofGetHeight() - dimension) / 2);
+	ofSetColor(r_pt, g_pt, b_pt);
+	ofFill();
+	for (size_t i = 0; i < num_pts; i++) {
+		ofCircle(points[i], 2);
+	}
+	ofPopMatrix();
+}
+
+void ofApp::setupPointCoord() {
+	int index = 0;
+	for (size_t i = 0; i < sqrt(num_pts); i++) {
+		for (size_t j = 0; j < sqrt(num_pts); j++) {
+			points[index].set(i * pt_spacing, j * pt_spacing);
+			index++;
+		}
+	}
 }
--- src/ofApp.h
+++ src/ofApp.h
@@ -3,8 +3,13 @@
 #include "ofMain.h"
 
 class ofApp : public ofBaseApp {
+	int r_bg = 175, g_bg = 175, b_bg = 175;
+	int r_pt = 0, g_pt = 0, b_pt = 0;
+
 public:
 	void setup();
 	void update();
 	void draw();
+	void drawPoints();
+	void setupPointCoord();
 };
