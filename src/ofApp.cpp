f4857b692aa62b124c62c3696f397951bb7f3d90
Leonardo <ll10@illinois.edu>
2018-04-16 14:16:46 -0500

    Created Dancing Cloud

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,40 +1,79 @@
 #include "ofApp.h"
 
-const int num_pts = 324;
+const int num_pts = 250;
+vector<double> y_off(num_pts), x_off(num_pts);
 ofVec2f points[num_pts];
-int pt_spacing = 15;
+vector<bool> is_connected(num_pts, false);
+
 
 void ofApp::setup() {
-	setupPointCoord();
+	for (size_t i = 0; i < num_pts; i++) {
+		x_off[i] = ofRandom(0, 100);
+		y_off[i] = ofRandom(0, 100);
+	}
 }
 
 void ofApp::update() {
-
+	updatePoints();
 }
 
 void ofApp::draw() {
-	ofBackground(r_bg, g_bg, b_bg);
+	ofBackground(bg_color->r, bg_color->g, bg_color->b);
 	drawPoints();
 }
 
+//Point Setup, Update, and Draw for Dancing Cloud
+
 void ofApp::drawPoints() {
-	int dimension = pt_spacing * (sqrt(num_pts));
+	// Center points
+	// https://stackoverflow.com/questions/12516550/openframeworks-rotate-an-image-from-its-center-through-opengl-calls
 	ofPushMatrix();
-	ofTranslate((ofGetWidth() - dimension) / 2, (ofGetHeight() - dimension) / 2);
-	ofSetColor(r_pt, g_pt, b_pt);
+	ofTranslate(ofGetWidth() / 2, ofGetHeight() / 2);
+	
+	//Set color of points and create circles at those points
+	ofSetColor(pt_color->r, pt_color->g, pt_color->b);
 	ofFill();
 	for (size_t i = 0; i < num_pts; i++) {
-		ofCircle(points[i], 2);
+		ofDrawCircle(points[i], 2);
 	}
+	connectPoints();
 	ofPopMatrix();
 }
 
-void ofApp::setupPointCoord() {
-	int index = 0;
-	for (size_t i = 0; i < sqrt(num_pts); i++) {
-		for (size_t j = 0; j < sqrt(num_pts); j++) {
-			points[index].set(i * pt_spacing, j * pt_spacing);
-			index++;
+//Connects points within a certain distance of each other
+void ofApp::connectPoints() {
+	for (size_t i = 0; i < num_pts; i++) {
+		for (size_t j = i + 1; j < num_pts; j++) {
+			double dist = ofDist(points[i].x, points[i].y, points[j].x, points[j].y);
+			if (dist < distance_threshold) {
+				is_connected[i] = true;
+				if (is_connected[i]) {
+					ofSetColor(ofRandom(255));
+				}
+				else {
+					ofSetColor(0, 0, 0);
+				}
+				ofDrawLine(points[i], points[j]);
+			}
+			is_connected[i] = false;
 		}
+	}
+}
+
+void ofApp::updatePoints() {
+	double elapsed_time = ofGetElapsedTimef();
+
+	//Change in time since last update()
+	double dt = elapsed_time - curr_time;
+	curr_time = elapsed_time;
+
+	for (size_t i = 0; i < num_pts; i++) {
+		//Get distance travelled
+		y_off[i] += pt_velocity * dt;
+		x_off[i] += pt_velocity * dt;
+
+		//Update position using Perlin Noise 
+		points[i].x = ofSignedNoise(x_off[i]) * cloud_radius;
+		points[i].y = ofSignedNoise(y_off[i]) * cloud_radius;
 	}
 }
--- src/ofApp.h
+++ src/ofApp.h
@@ -3,13 +3,25 @@
 #include "ofMain.h"
 
 class ofApp : public ofBaseApp {
-	int r_bg = 175, g_bg = 175, b_bg = 175;
-	int r_pt = 0, g_pt = 0, b_pt = 0;
+	struct Color {
+		Color(int red, int green, int blue) : r(red), g(green), b(blue){};
+		int r;
+		int g;
+		int b;
+	};
+	Color *bg_color = new Color(175, 175, 175);
+	Color *pt_color = new Color(0, 0, 0);
+
+	int distance_threshold = 50;
+	double pt_velocity = .05;
+	int cloud_radius = 500;
+	double curr_time = 0;
 
 public:
 	void setup();
 	void update();
 	void draw();
 	void drawPoints();
-	void setupPointCoord();
+	void updatePoints();
+	void connectPoints();
 };
